1cefc655a94d7e2375f41216deae2aa7048913e8
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2023-06-30 16:01:36 -0300

    :recycle: refactor: color text in tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const html = document.querySelector('html');
 const checkbox = document.getElementById('switch');
 
+html.classList.toggle('dark-mode');
+
 checkbox.addEventListener('change', () => {
     html.classList.toggle('dark-mode');
 })
